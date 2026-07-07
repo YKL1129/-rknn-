054de11860e18cc233b0f4e8468fa0ef5144489d
澈 <2137144545@qq.com>
2026-07-07 20:47:05 +0800

    Fix missing label path in PC training

--- PC_MLP_System.py
+++ PC_MLP_System.py
@@ -57,6 +57,7 @@ dashscope.api_key = ""
 DATA_PATH = os.path.join('CNN_Gesture-master/MP_Data_Fusion')
 MODEL_PATH_H5 = 'CNN_Gesture-master/action_fusion.h5'
 MODEL_PATH_ONNX = 'CNN_Gesture-master/action_fusion.onnx'
+LABELS_PATH = 'CNN_Gesture-master/labels.txt'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 RUNTIME_CONFIG = load_runtime_config(BASE_DIR)
 MASTER_ROOT = os.path.join(BASE_DIR, RUNTIME_CONFIG["master_root"])
